ad8e140d8431e10d37ae288299e78c50409b98ad
AdrianTapiaD <adrian.tapia.donoso@gmail.com>
2024-03-05 16:53:41 -0300

    v 0.5

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function buscarSuperhero() {
+    var numeroSuperhero = $('#numero').val();
+
+    $.ajax({
+        url: 'https://superheroapi.com/api/4905856019427443/' + numeroSuperhero,
+        type: 'GET',
+        success: function(response) {
+            console.log('API Response:', response);
+
+            if (response.response === 'success') {
+                console.log('Full API Response:', response);
+
+                var nombreSuperhero = response.name;
+                var imgURL = response.image.url;
+
+                $('#respuesta').html('<p>Nombre del Superhero: ' + nombreSuperhero + '</p>' + '<img src="' + imgURL + '" alt="' + nombreSuperhero + '">');
+            } else {
+                $('#respuesta').text('No se encontró Superhero');
+            }
+        },
+        error: function(error) {
+            console.error('Error al recibir información desde API:', error);
+            $('#respuesta').text('Tu resultado es:');
+        }
+    });
+}
